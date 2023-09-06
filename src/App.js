aef69de70028506bf5d68c4f3aa481ca285bd559
aluno <aluno@ifpe>
2023-09-06 11:22:34 -0300

    cadastro com back

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './views/home/home';
-import FormCliente from './views/cliente/FormCliente';
+import FormProduto from './views/produto/FormProduto';
+import Rotas from './rotas';
+import { Segment } from 'semantic-ui-react';
 
 function App() {
   return (
-   <FormCliente />
+  
+    <div className="App">
+
+    <Rotas />
+
+    <div style={{marginTop: '6%'}}>
+      <Segment vertical color='grey' size='tiny' textAlign='center'>
+        &copy; 2023 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+      </Segment>
+    </div>
+
+  </div>
+
   );
 }
 
